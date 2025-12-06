b7c6f131607f0bc0f991bbc6cf1a8970e16168a7
JMS-tesoy <fxjoel237@gmail.com>
2025-12-06 20:18:38 +0800

    Stop tracking .next build folder

--- .next/dev/static/chunks/app_login_page_tsx_38e5af12._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_69d47eaf._.js",
-  "static/chunks/_c6dae2c6._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_register_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_80a6bc9c._.js",
-  "static/chunks/_45635e18._.js"
-],
-    source: "dynamic"
-});
